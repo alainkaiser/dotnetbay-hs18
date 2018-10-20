a42e2bb4a8dfa42a3a62c1ae836a697291d08558
Alain Kaiser <kaiseralain1@gmail.com>
2018-10-20 16:37:07 +0200

    added value converter for auction state

---
+++ source/DotNetBay.WPF/Helper/AuctionStateConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DotNetBay.WPF.Helper
+{
+    public class AuctionStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = "Offen";
+            if (!(bool)value)
+            {
+                result = "Abgeschlossen";
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = true;
+            if (((string)value).ToLower() == "abgeschlossen")
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
